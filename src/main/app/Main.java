90a083fc30c5ca2254916a19a8c8322e4c0e7a12
gitlyssa <121577615+gitlyssa@users.noreply.github.com>
2023-11-11 16:36:10 -0500

    displayed interactive map using OpenStreetMaps

--- src/main/app/Main.java
+++ src/main/app/Main.java
@@ -1,5 +1,12 @@
 package main.app;
 
+import org.jdesktop.swingx.JXMapKit;
+import org.jdesktop.swingx.JXMapViewer;
+import org.jdesktop.swingx.mapviewer.DefaultWaypoint;
+import org.jdesktop.swingx.mapviewer.GeoPosition;
+import org.jdesktop.swingx.mapviewer.Waypoint;
+
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -14,10 +21,22 @@ public class Main {
 
         CardLayout cardLayout = new CardLayout();
 
+        // Displaying interactive map
+        JXMapKit jxMapKit = new JXMapKit();
+        jxMapKit.setDefaultProvider(JXMapKit.DefaultProviders.OpenStreetMaps);
+        jxMapKit.setDataProviderCreditShown(true);
+        jxMapKit.setZoom(5);
+        jxMapKit.setAddressLocationShown(true);
+
+        // Set starting point at UofT
+        GeoPosition waypoint1 = new GeoPosition(43.6634425, -79.3964002);
+        jxMapKit.getMainMap().setAddressLocation(waypoint1);
+
         // The various View objects. Only one view is visible at a time.
         JPanel views = new JPanel(cardLayout);
         application.add(views);
-
+        views.add(jxMapKit, "Map");
+        application.setMinimumSize(new Dimension(1000, 600));
         application.pack();
         application.setVisible(true);
     }
