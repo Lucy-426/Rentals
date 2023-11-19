3ea842e56643df7fca9e64981238970dc6765dd9
Lucy Shen <lucyshen26@gmail.com>
2023-11-19 16:55:52 -0500

    adding in preliminary code for api call

--- src/data_access/PropertyDataAccessObject.java
+++ src/data_access/PropertyDataAccessObject.java
@@ -1,4 +1,23 @@
 package data_access;
 
+import entity.Property;
+import entity.PropertyFactory;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class PropertyDataAccessObject {
+    private final File csvFile;
+
+    private final Map<String, Integer> headers = new LinkedHashMap<>();
+
+    private final Map<String, Property> properties = new HashMap<>();
+
+    private PropertyFactory propertyFactory;
+
+    public PropertyDataAccessObject() {
+
+    }
 }
--- src/main/app/api/Rentals.java
+++ src/main/app/api/Rentals.java
@@ -12,6 +12,13 @@ public class Rentals {
     private static final String API_URL = "https://api.gateway.attomdata.com/propertyapi/v1.0.0/";
     private static final String API_TOKEN = "ea2ec50d07946af90b2090156a03e236";
 
+
+    // Request 1: get the list of listings from longitude and latitude search
+    // Request 2: find the details for each listing from request 1 based on attom id (see which is the best way to do this
+    // If the first endpoint (assessement) can give both listings and details then just one request is good
+    // If not, look into property basic profile or profile detail (find the one that is most detailed)
+    // Then create csv file and save listings (figure out the columns)
+
     public static void main(String[] args) {
         OkHttpClient client = new OkHttpClient();
 
