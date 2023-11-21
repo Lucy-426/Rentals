3e838ff32dce937e8f4fd6d6f65eeffacc0e626b
erinkim16 <125765924+erinkim16@users.noreply.github.com>
2023-11-20 19:47:20 -0500

    added filter method in data access interface and wrote pseudocode for the filter method

--- src/data_access/InMemoryDataAccessObject.java
+++ src/data_access/InMemoryDataAccessObject.java
@@ -16,21 +16,22 @@ public class InMemoryDataAccessObject implements HomeSearchDataAccessInterface {
     private final Map<String, Integer> headers = new LinkedHashMap<>();
 
     private final Map<String, ArrayList> property_chars = new HashMap<>();
+    private final Map<String, ArrayList> filtered_properties = new HashMap<>();
     private PropertyFactory propertyFactory;
 
-//    might need to throw IO Exception?
 public InMemoryDataAccessObject(String csvPath) throws IOException {
 //    this.propertyFactory = propertyFactory;
 
     csvFile = new File(csvPath);
     headers.put("id", 0);
-    headers.put("address", 1);
-    headers.put("numRooms", 2);
-    headers.put("priceRange", 3);
-    headers.put("numBaths", 4);
-    headers.put("walkScore", 5);
-    headers.put("furnished", 6);
-    headers.put("listingType", 7);
+    headers.put("city", 1);
+    headers.put("address", 2);
+    headers.put("numRooms", 3);
+    headers.put("priceRange", 4);
+    headers.put("numBaths", 5);
+    headers.put("walkScore", 6);
+    headers.put("furnished", 7);
+    headers.put("listingType", 8);
 
 //    I don't think we need this if statement?
 //    if (csvFile.length() == 0) {
@@ -47,6 +48,7 @@ public InMemoryDataAccessObject(String csvPath) throws IOException {
             while ((row = reader.readLine()) != null) {
                 String[] col = row.split(",");
                 String id = String.valueOf(col[headers.get("id")]);
+                String city = String.valueOf(col[headers.get("city")]);
                 String address = String.valueOf(col[headers.get("address")]);
                 String numRooms = String.valueOf(col[headers.get("numRooms")]);
                 String priceRange = String.valueOf(col[headers.get("priceRange")]);
@@ -57,6 +59,7 @@ public InMemoryDataAccessObject(String csvPath) throws IOException {
 
                 ArrayList<String> characteristics = new ArrayList<String>();
                 characteristics.add(address);
+                characteristics.add(city);
                 characteristics.add(numRooms);
                 characteristics.add(priceRange);
                 characteristics.add(numBaths);
@@ -70,9 +73,23 @@ public InMemoryDataAccessObject(String csvPath) throws IOException {
     }
 }
 
-
+// this save method will be implemented when we combine this code with the code in PropertyDataAccessObject file.
+    @Override
     public void save(Property property) {
-//        properties.put(property.getAddress(), property);
+
+    }
+
+    public void filter(HashMap property_chars) {
+        filtered_properties.putAll(property_chars);
+// my pseudocode:
+//        for property in filtered_properties:
+//          if (city != null or city != "all" or city != property.city)
+//               filtered_properties.remove(property)
+//          if (address != null or address != "all" or address != property.address)
+//               filtered_properties.remove(property)
+//          if (numRooms != null or numRooms != "all" or numRooms != property.numRooms)
+//               filtered_properties.remove(property)
+//        and so on...
     }
 
 }
