5f105712e306cf4a00ce3bec2488f51cbe576be2
gitlyssa <121577615+gitlyssa@users.noreply.github.com>
2023-11-26 20:34:27 -0500

    completed helper function that calculates the distance b/w two points

--- src/entity/DistanceCalculation.java
+++ src/entity/DistanceCalculation.java
@@ -8,30 +8,35 @@ import com.google.maps.model.TravelMode;
 import java.util.concurrent.TimeUnit;
 
 public class DistanceCalculation {
-    public static long calculation(double startLat, double startLng, double endLat, double endLng) {
+    public static double calculation(double startLat, double startLng, double endLat, double endLng) {
         String apiKey = "AIzaSyAMz9doGhcdEYjPoXY3Cv4TCd58-eHDubU";
 
+        // Set up the GeoApiContext with API key
+        GeoApiContext context = new GeoApiContext.Builder().apiKey(apiKey).build();
+
+        // Specify the origin and destination coordinates
+        String origin = startLat + "," + startLng;
+        String destination = endLat + "," + endLng;
+
         try {
-            GeoApiContext context = new GeoApiContext.Builder().apiKey(apiKey).build();
+            // Make a query to get the walking distance in kilometers
+            DirectionsResult result = DirectionsApi.newRequest(context)
+                    .origin(origin)
+                    .destination(destination)
+                    .mode(TravelMode.WALKING)
+                    .await();
 
-            // Create a directions request
-            DirectionsApiRequest request = DirectionsApi.newRequest(context)
-                    .origin(new com.google.maps.model.LatLng(startLat, startLng))
-                    .destination(new com.google.maps.model.LatLng(endLat, endLng))
-                    .mode(TravelMode.WALKING); // You can change the travel mode if needed
-
-            // Execute the request
-            DirectionsResult result = request.await();
-
-            // Get the total distance in meters
-            long distanceInMeters = result.routes[0].legs[0].distance.inMeters;
-
-            // Print the result
-            return distanceInMeters;
-
+            // Extract and return the walking distance
+            double distance = result.routes[0].legs[0].distance.inMeters;
+            return distance/1000;
         } catch (Exception e) {
             e.printStackTrace();
+            return 0;
         }
-        return 1;
+    }
+
+    // Test from Bahen to Eaton - delete at the end
+    public static void main(String[] args) {
+        calculation(43.6598084,-79.3998729,43.6544421,-79.3832743);
     }
 }
