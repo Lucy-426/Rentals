48171e28df0d623598367bac4dc5a3751df25a9e
Alicia <aliciaz3136@gmail.com>
2023-11-27 02:25:37 -0500

    edited entities

--- src/entity/Listing.java
+++ src/entity/Listing.java
@@ -8,20 +8,18 @@ public class Listing {
     private final int walkScore;
     private final int numBed;
     private final int numBath;
-    private final int squareFeet;
     private final boolean furnished;
     private final boolean parking;
     private final String contact;
 
-    public Listing(String rentalType, int daysListedAgo, float price, int walkScore, int numBed, int numBath,
-                    int squareFeet, boolean furnished, boolean parking, String contact) {
+    public Listing(String rentalType, int daysListedAgo, float price, int walkScore, int numBed,
+                   int numBath, boolean furnished, boolean parking, String contact) {
         this.rentalType = rentalType;
         this.daysListedAgo = daysListedAgo;
         this.price = price;
         this.walkScore = walkScore;
         this.numBed = numBed;
         this.numBath = numBath;
-        this.squareFeet = squareFeet;
         this.furnished = furnished;
         this.parking = parking;
         this.contact = contact;
@@ -58,10 +56,6 @@ public class Listing {
 
     public float getPrice() {
         return price;
-    }
-
-    public int getSquareFeet() {
-        return squareFeet;
     }
 
     public int getWalkScore() {
