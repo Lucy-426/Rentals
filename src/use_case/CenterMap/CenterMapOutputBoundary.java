607c8b919b243aa8cd91be89c95e3fd9f3f5445c
gitlyssa <121577615+gitlyssa@users.noreply.github.com>
2023-11-28 15:20:12 -0500

    completed write up of output boundary and renamed file

--- src/use_case/CenterMap/CenterMapOutputBoundary.java
+++ src/use_case/CenterMap/CenterMapOutputBoundary.java
@@ -1,6 +1,6 @@
-package use_case.search;
+package use_case.CenterMap;
 
-public interface SearchOutputBoundary {
-    void prepareSuccessView(SearchOutputData searchOutputData);
-    void prepareFailView(SearchOutputData searchOutputData);
+public interface CenterMapOutputBoundary {
+    void prepareSuccessView(CenterMapOutputData centerMapOutputData);
+    void prepareFailView(String error);
 }
