8836e057d58cf1552ba5bbe5635a9d5c915b4853
gitlyssa <121577615+gitlyssa@users.noreply.github.com>
2023-11-28 15:46:13 -0500

    added important note on exists function

--- src/data_access/MapDataAccessObject.java
+++ src/data_access/MapDataAccessObject.java
@@ -26,20 +26,27 @@ public class MapDataAccessObject implements CenterMapDataAccessInterface {
 
     @Override
     public boolean exists (String address) {
-//        GeoApiContext context = new GeoApiContext.Builder().apiKey(MAPS_API_KEY).build();
-//
-//        try {
-//            // Perform geocoding to get the location information
-//            GeocodingResult[] results = GeocodingApi.geocode(context, address).await();
-//
-//            // Check if there is at least one result and that it contains lat/lng coordinates
-//            return results[0].geometry.location != null;
-//
-//        } catch (Exception e) {
-//            // Not a valid address
-//            e.printStackTrace();
-//            return false;
-//        }
+        /*
+        NOTE: The Google Maps API says I don't have permission to validate addresses, so
+        I'll address this function again later after completion of other important aspects
+        of the project. For now, if you type an invalid address, you will be redirected
+        to a random point on a map and an error will show in the terminal.
+         */
+
+        /*GeoApiContext context = new GeoApiContext.Builder().apiKey(MAPS_API_KEY).build();
+
+        try {
+            // Perform geocoding to get the location information
+            GeocodingResult[] results = GeocodingApi.geocode(context, address).await();
+
+            // Check if there is at least one result and that it contains lat/lng coordinates
+            return results[0].geometry.location != null;
+
+        } catch (Exception e) {
+            // Not a valid address
+            e.printStackTrace();
+            return false;
+        }*/
         return true;
     }
 
@@ -97,10 +104,5 @@ public class MapDataAccessObject implements CenterMapDataAccessInterface {
             e.printStackTrace();
             return 0;
         }
-    }
-
-    @Override
-    public void centerMap(double lat, double lng) {
-
     }
 }
