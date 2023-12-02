42dad1446cbf05522845f02e7b7f450b9f1cbf59
gitlyssa <121577615+gitlyssa@users.noreply.github.com>
2023-12-02 15:35:40 -0500

    added set of waypoints to display as output data

--- src/use_case/home/HomeOutputData.java
+++ src/use_case/home/HomeOutputData.java
@@ -1,15 +1,25 @@
 package use_case.home;
 
+import org.jdesktop.swingx.mapviewer.Waypoint;
+
 import java.util.HashMap;
+import java.util.Set;
 
 public class HomeOutputData {
     private final HashMap<String, String> displayedProperties;
 
-    public HomeOutputData(HashMap<String, String> displayedProperties) {
+    private final Set<Waypoint> waypoints;
+
+    public HomeOutputData(HashMap<String, String> displayedProperties, Set<Waypoint> waypoints) {
         this.displayedProperties = displayedProperties;
+        this.waypoints = waypoints;
     }
 
     public HashMap<String, String> getDisplayedProperties() {
         return displayedProperties;
+    }
+
+    public Set<Waypoint> getWaypoints() {
+        return waypoints;
     }
 }
