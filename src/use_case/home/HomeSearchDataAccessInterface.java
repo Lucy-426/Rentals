d776672d91c6c18934c3c0de1b2ec450a84c098a
gitlyssa <121577615+gitlyssa@users.noreply.github.com>
2023-12-02 15:34:25 -0500

    modified to add methods to add waypoints on the map

--- src/use_case/home/HomeSearchDataAccessInterface.java
+++ src/use_case/home/HomeSearchDataAccessInterface.java
@@ -1,8 +1,11 @@
 package use_case.home;
 
 import entity.Property;
+import org.jdesktop.swingx.mapviewer.Waypoint;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public interface HomeSearchDataAccessInterface {
 
@@ -14,4 +17,12 @@ public interface HomeSearchDataAccessInterface {
     HashMap<String, String> getFilteredProperties();
 
     Property getProperty(String id);
+
+    /*Converting set of properties into set of its corresponding waypoints
+    with the latitude/longitude coordinates*/
+    Set<Waypoint> getCoordinates(HashMap<String, String> properties);
+
+    double getLat(String address);
+
+    double getLong(String address);
 }
