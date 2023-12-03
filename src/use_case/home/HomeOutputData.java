bfc6665488be7c74207a964470249b954e53c0b3
gitlyssa <121577615+gitlyssa@users.noreply.github.com>
2023-12-03 11:35:30 -0500

    modified to add waypointToID hashmap

--- src/use_case/home/HomeOutputData.java
+++ src/use_case/home/HomeOutputData.java
@@ -8,15 +8,22 @@ import java.util.Set;
 public class HomeOutputData {
     private final HashMap<String, String> displayedProperties;
 
+    private final HashMap<Waypoint, String> waypointIDMap;
+
     private final Set<Waypoint> waypoints;
 
-    public HomeOutputData(HashMap<String, String> displayedProperties, Set<Waypoint> waypoints) {
+    public HomeOutputData(HashMap<String, String> displayedProperties, HashMap<Waypoint, String> waypointIDMap, Set<Waypoint> waypoints) {
         this.displayedProperties = displayedProperties;
+        this.waypointIDMap = waypointIDMap;
         this.waypoints = waypoints;
     }
 
     public HashMap<String, String> getDisplayedProperties() {
         return displayedProperties;
+    }
+
+    public HashMap<Waypoint, String> getWaypointIDMap() {
+        return waypointIDMap;
     }
 
     public Set<Waypoint> getWaypoints() {
