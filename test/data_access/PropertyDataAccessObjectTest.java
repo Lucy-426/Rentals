b05cdcd713b7e83d79b977cb94e554af29a68ae7
erinkim16 <125765924+erinkim16@users.noreply.github.com>
2023-12-03 20:36:28 -0500

    made test for property entity

--- test/data_access/PropertyDataAccessObjectTest.java
+++ test/data_access/PropertyDataAccessObjectTest.java
@@ -1,10 +1,19 @@
 package data_access;
 
+import entity.Property;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PropertyDataAccessObjectTest {
+    private Map<String, Property> properties;
+    @BeforeEach
+    void setUp() {
+    }
 
     @Test
     void save() {
