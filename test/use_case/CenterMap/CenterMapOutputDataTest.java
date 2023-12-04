54f87942cf1806f89c970cf8fd8c05f2ec56f5cc
gitlyssa <121577615+gitlyssa@users.noreply.github.com>
2023-12-03 23:09:13 -0500

    wrote tests for center map output data

--- test/use_case/CenterMap/CenterMapOutputDataTest.java
+++ test/use_case/CenterMap/CenterMapOutputDataTest.java
@@ -1,4 +1,16 @@
+package use_case.CenterMap;
+
+import org.jdesktop.swingx.mapviewer.GeoPosition;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CenterMapOutputDataTest {
-  
+
+    @Test
+    void getGeoPosition() {
+        GeoPosition actual = new GeoPosition(43.6780371, -79.4094439);
+        CenterMapOutputData centerMapOutputData = new CenterMapOutputData(actual, false);
+        assertEquals(centerMapOutputData.getGeoPosition(), actual);
+    }
 }
